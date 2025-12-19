afc8dffbe3dd7875577881fff12854a28ff18a5d
suwenyung <suwenyung@gmail.com>
2025-12-19 12:19:41 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+const settings = [
+  {
+    default: [
+      {
+        style: [],
+        script: [{ settings.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/settings.js' }],
+      },
+    ],
+    user: [
+      {
+        style: [],
+        script: [{ user.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/user.js' }],
+      },
+    ],
+    apps: [
+      {
+        style: [],
+        script: [{ datepicker.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/datepicker.js' }],
+      },
+    ],
+    feeds: [
+      {
+        style: [],
+        script: [{ 1100.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/1100.js' },{ 909090.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909090.js' },{ 909091.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909091.js' },],
+      },
+    ],
+  },
+];
