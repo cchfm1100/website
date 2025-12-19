41ce7205d99c9d3f23925af3a5aa0f771ff4170b
suwenyung <suwenyung@gmail.com>
2025-12-19 16:50:19 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -3,25 +3,29 @@ const settings = [
     default: [
       {
         style: [],
-        script: [{ settings.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/settings.js' }],
+        script: [{ "settings.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/settings.js" }],
       },
     ],
     user: [
       {
         style: [],
-        script: [{ user.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/user.js' }],
+        script: [{ "user.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/user.js" }],
       },
     ],
     apps: [
       {
         style: [],
-        script: [{ datepicker.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/datepicker.js' }],
+        script: [{ "datepicker.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/datepicker.js" }],
       },
     ],
     feeds: [
       {
         style: [],
-        script: [{ 1100.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/1100.js' },{ 909090.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909090.js' },{ 909091.js: 'https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909091.js' },],
+        script: [
+          { "1100.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/1100.js" },
+          { "909090.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909090.js" },
+          { "909091.js": "https://cdn.jsdelivr.net/gh/cchfm1100/website@main/909091.js" }
+        ],
       },
     ],
   },
